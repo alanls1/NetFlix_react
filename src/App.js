fafcb43e1b0c2ffafc3a86029b154c4221eb53f8
alan lima <alanlima4123@gmail.com>
2022-07-25 11:29:26 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React,{useEffect, useState} from "react";
+import './App.css'
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
+
+
+
+export default ()=>{
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData,setFeaturedData] = useState(null);
+  const [blackHeader,setBlackHeader]=useState(false);
+
+  useEffect(()=>{
+    const loadAll=async()=>{
+      //pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random()* (originals[0].items.results.length));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData(chosenInfo);
+    }
+  
+    loadAll();
+  },[]);
+
+  useEffect(()=>{
+    const scrollListener = ()=>{
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll',scrollListener);
+    return ()=>{
+      window.removeEventListener('scroll',scrollListener);
+    }
+  },[]);
+
+  return(
+    <div className="page">
+
+      <Header black={blackHeader}/>
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+        <section className="lists">
+          {movieList.map((item,key)=>(
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          ))}
+        </section>
+
+        <footer>
+          Feito com pela B7web <br/>
+          Direitos de imagem para netFlix <br/>
+          Dados pegos do site themoviedb.org
+        </footer>
+        
+    </div>
+  );
+} 
